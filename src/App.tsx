fdc4a62589ebfca14ef758ef8460282596045863
soongu <naong526@naver.com>
2024-08-19 17:43:39 +0900

    06 useRef, event, useState 처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
+import { useState } from "react";
+import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
 
 function App() {
 
-  const todos = [
-    new Todo('Learning React'),
-    new Todo('Learning Typescript'),
-    new Todo('Learning Redux'),
-  ];
+  // const todos = [
+  //   new Todo('Learning React'),
+  //   new Todo('Learning Typescript'),
+  //   new Todo('Learning Redux'),
+  // ];
+
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+    setTodos((prevState) => [...prevState, newTodo]);
+  };
 
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,38 @@
+import React, { useRef } from 'react';
+
+type Props = {
+  onAddTodo: (todoText: string) => void
+};
+
+const NewTodo = ({ onAddTodo }: Props) => {
+
+  const todoRef = useRef<HTMLInputElement>(null);
+
+  // const submitHandler = (e: React.MouseEvent) => { // 에러
+  const submitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // ! 는 절대로 이시점에서는 current가 null이 아님을 확인시켜줌
+    const enteredText = todoRef.current!.value;
+
+    if (enteredText.trim().length === 0) {
+      return;
+    }
+
+    onAddTodo(enteredText);
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <label htmlFor="text">Todo Text</label>
+      <input
+        ref={todoRef}
+        id="text"
+        type="text"
+      />
+      <button>Add Todo</button>
+    </form>
+  );
+};
+
+export default NewTodo;
